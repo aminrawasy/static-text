9661500e2bb64643c5604506c76074cfae9e0ceb
Amin <ahmed.amin1@theelephant.tech>
2023-07-10 12:39:13 +0300

    #first push

--- src/static_text/static_text.controller.ts
+++ src/static_text/static_text.controller.ts
@@ -1,24 +1,9 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Param, Delete } from '@nestjs/common';
 import { StaticTextService } from './static_text.service';
-import { CreateStaticTextDto } from './dto/create-static_text.dto';
-import { UpdateStaticTextDto } from './dto/update-static_text.dto';
 
 @Controller('static-text')
 export class StaticTextController {
   constructor(private readonly staticTextService: StaticTextService) {}
-
-  @Post()
-  create(@Body() createStaticTextDto: CreateStaticTextDto) {
-    return this.staticTextService.create(createStaticTextDto);
-  }
 
   @Get()
   findAll() {
@@ -28,14 +13,6 @@ export class StaticTextController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.staticTextService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateStaticTextDto: UpdateStaticTextDto,
-  ) {
-    return this.staticTextService.update(+id, updateStaticTextDto);
   }
 
   @Delete(':id')
